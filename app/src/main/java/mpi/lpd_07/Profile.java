6b020e61e9a53bb0ff4fc564ab046ee6dbbb4312
Karlis Jansons <karlisj28@inbox.lv>
2017-04-26 23:00:35 +0300

    Pievienots Facebook vards

--- app/src/main/java/mpi/lpd_07/Profile.java
+++ app/src/main/java/mpi/lpd_07/Profile.java
@@ -3,9 +3,21 @@ package mpi.lpd_07;
 import android.hardware.camera2.params.Face;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import org.json.JSONObject;
 
 public class Profile extends AppCompatActivity {
 
@@ -15,11 +27,12 @@ public class Profile extends AppCompatActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_profile);
         try{
-            String Vards = com.facebook.Profile.getCurrentProfile().getFirstName() + " " + com.facebook.Profile.getCurrentProfile().getLastName();
-            TextView textView = (TextView) findViewById(R.id.tv_p_vards);
-            textView.setText(Vards);
+            String Vards = com.facebook.Profile.getCurrentProfile().getName();
+            TextView VardsUz = (TextView) findViewById(R.id.tv_p_vards);
+            VardsUz.setText(Vards);
         }catch (Exception e){
-
+            Toast toast = Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT);
+            toast.show();
         }
 
     }
