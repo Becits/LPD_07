1c29d75d1bb542bdf25088ee250ae6d4e7436318
Karlis Jansons <karlisj28@inbox.lv>
2017-04-26 22:16:32 +0300

    Pievienots vards no Facebook

--- app/src/main/java/mpi/lpd_07/Profile.java
+++ app/src/main/java/mpi/lpd_07/Profile.java
@@ -1,13 +1,26 @@
 package mpi.lpd_07;
 
+import android.hardware.camera2.params.Face;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.FacebookSdk;
 
 public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_profile);
+        try{
+            String Vards = com.facebook.Profile.getCurrentProfile().getFirstName() + " " + com.facebook.Profile.getCurrentProfile().getLastName();
+            TextView textView = (TextView) findViewById(R.id.tv_p_vards);
+            textView.setText(Vards);
+        }catch (Exception e){
+
+        }
+
     }
 }
