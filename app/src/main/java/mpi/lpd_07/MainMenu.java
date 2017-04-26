b24f3f80b0ddd0039f3e07b186c24a70c1237eb2
Becits <becic@inbox.lv>
2017-04-26 20:31:28 +0300

    First commit

---
+++ app/src/main/java/mpi/lpd_07/MainMenu.java
@@ -0,0 +1,46 @@
+package mpi.lpd_07;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    // Poga "Izveidot pasaakumu"
+    public void buttonIzveidot(View view){
+        Intent nIntent = new Intent(MainMenu.this, NewEvent.class);
+        startActivity(nIntent);
+    }
+
+    // Poga "Pasaakumi"
+    public void buttonPasakumi(View view){
+        Intent nIntent = new Intent(MainMenu.this, Events.class);
+        startActivity(nIntent);
+    }
+
+    // Poga "Draugi"
+    public void buttonDraugi(View view){
+        Intent nIntent = new Intent(MainMenu.this, Friends.class);
+        startActivity(nIntent);
+    }
+
+    // Poga "Profils"
+    public void buttonProfils(View view){
+        Intent nIntent = new Intent(MainMenu.this, Profile.class);
+        startActivity(nIntent);
+    }
+
+    // Poga "Iziet"
+    public void buttonIziet(View view){
+        Intent nIntent = new Intent(MainMenu.this, Login.class);
+        startActivity(nIntent);
+    }
+
+}
