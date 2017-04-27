39ea4aef73e065c38e3b47a2166418aaf49dd159
Karlis Jansons <karlisj28@inbox.lv>
2017-04-27 04:11:54 +0300

    Izveidota dokumenta struktūra

--- app/src/main/java/mpi/lpd_07/Gallery.java
+++ app/src/main/java/mpi/lpd_07/Gallery.java
@@ -2,11 +2,14 @@ package mpi.lpd_07;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Gallery extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+        Toast toast = new Toast.makeText(getApplicationContext(), "Toast", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
